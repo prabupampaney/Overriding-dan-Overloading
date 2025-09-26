9cc5279fe14412570fc4044db23d23c307958cfa
prabupampaney <prabupampan@gmail.com>
2025-09-26 07:34:30 +0700

    Overriding Dan Overloading

---
+++ BangunDatar.java
@@ -0,0 +1,14 @@
+package overriding;
+public class BangunDatar {
+public double luas, keliling;
+public void hitungLuas(){
+luas = 0;
+}
+public void hitungKeliling(){
+keliling = 0;
+}
+public void tampilData(){
+System.out.println("LUAS = "+luas);
+System.out.println("KELILING = "+keliling);
+}
+}
